6d20e8d01090ed693df045d69614b1e557e5d74e
walter <wjesus88@gmail.com>
2018-04-17 17:27:19 +0800

    o

---
+++ index.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const env = require('dotenv').config();
+const express = require('express');
+var server = express();
+
+
+server.set('view engine', 'jade');
+server.set('views', 'views');
+var port = process.env.PORT || 8080;
+
+//if (process.env.NODE_ENV === 'development') {
+    // if (process.env.MODE === 'no-build') {
+    //     server.use(express.static(path.join(__dirname, 'apps/shop')));
+    // }
+    // else if (process.env.MODE === 'build') {
+    //     server.use(express.static(path.join(__dirname, 'apps/shop/build/bundled')));
+    // }
+    // server.use(express.static(path.join(__dirname, 'static')));
+//}
+
+
+server.get('/', (req, res) => {
+    // if (process.env.NODE_ENV === 'developemnt') {
+    //     res.render('shop', {mode: process.env.MODE});
+    // }
+    // else {
+        res.render('shop');
+    //}
+});
+
+
+server.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+})
