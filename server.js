7171d481f2e52569f44332aa6a51bee8412bbdc7
walter <wjesus88@gmail.com>
2018-04-17 18:43:54 +0800

    d

--- server.js
+++ server.js
@@ -8,26 +8,24 @@ var port = process.env.PORT || 8080;
 server.set('view engine', 'ejs');
 server.set('views', 'views');
 
-//if (process.env.NODE_ENV === 'development') {
-    // if (process.env.MODE === 'no-build') {
-    //     server.use(express.static(path.join(__dirname, 'apps/shop')));
-    // }
-    // else if (process.env.MODE === 'build') {
-    //     server.use(express.static(path.join(__dirname, 'apps/shop/build/bundled')));
-    // }
-    // server.use(express.static(path.join(__dirname, 'static')));
-//}
+if (process.env.NODE_ENV === 'development') {
+    if (process.env.MODE === 'no-build') {
+        server.use(express.static(path.join(__dirname, 'apps/shop')));
+    }
+    else if (process.env.MODE === 'build') {
+        server.use(express.static(path.join(__dirname, 'apps/shop/build/bundled')));
+    }
+    server.use(express.static(path.join(__dirname, 'static')));
+}
 
 
 server.get('/', (req, res) => {
-    // if (process.env.NODE_ENV === 'developemnt') {
-    //     res.render('shop', {mode: process.env.MODE});
-    // }
-    // else {
+    if (process.env.NODE_ENV === 'developemnt') {
+         res.render('shop', {mode: process.env.MODE});
+    }
+     else {
         res.render('index');
-    
-
-    //}
+    }
 });
 
 
