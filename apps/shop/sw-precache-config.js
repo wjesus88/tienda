fd20773c8e29c5c324298c60b9f15b994f73aacf
walter <wjesus88@gmail.com>
2018-04-18 12:05:40 +0800

    dd

--- apps/shop/sw-precache-config.js
+++ apps/shop/sw-precache-config.js
@@ -7,33 +7,33 @@ module.exports = {
   ],
   runtimeCaching: [
     {
-      urlPattern: /\/bower_components\/webcomponentsjs\/.*.js/,
-      handler: 'fastest',
-      options: {
-        cache: {
-          name: 'webcomponentsjs-polyfills-cache'
-        }
-      }
-    },
-    {
-      urlPattern: /\/data\/images\/.*/,
-      handler: 'cacheFirst',
-      options: {
-        cache: {
-          maxEntries: 200,
-          name: 'items-cache'
-        }
-      }
-    },
-    {
-      urlPattern: /\/data\/.*json/,
-      handler: 'fastest',
-      options: {
-        cache: {
-          maxEntries: 100,
-          name: 'data-cache'
-        }
-      }
-    }
-  ]
-};
+//       urlPattern: /\/bower_components\/webcomponentsjs\/.*.js/,
+//       handler: 'fastest',
+//       options: {
+//         cache: {
+//           name: 'webcomponentsjs-polyfills-cache'
+//         }
+//       }
+//     },
+//     {
+//       urlPattern: /\/data\/images\/.*/,
+//       handler: 'cacheFirst',
+//       options: {
+//         cache: {
+//           maxEntries: 200,
+//           name: 'items-cache'
+//         }
+//       }
+//     },
+//     {
+//       urlPattern: /\/data\/.*json/,
+//       handler: 'fastest',
+//       options: {
+//         cache: {
+//           maxEntries: 100,
+//           name: 'data-cache'
+//         }
+//       }
+//     }
+//   ]
+// };
