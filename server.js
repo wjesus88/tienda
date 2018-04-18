5d13e07f4ed11ae55eec4abb30a6b4fb0bc61642
walter <wjesus88@gmail.com>
2018-04-18 11:12:31 +0800

    r

--- server.js
+++ server.js
@@ -5,29 +5,29 @@ var server = express();
 
 var port = process.env.PORT || 8080;
 
-server.set('view engine', 'jade');
+server.set('view engine', 'ejs');
 server.set('views', 'views');
 
-if (process.env.NODE_ENV === 'development') {
-    if (process.env.MODE === 'no-build') {
-        server.use(express.static(path.join(__dirname, 'apps/shop')));
-    }
-    else if (process.env.MODE === 'build') {
-        server.use(express.static(path.join(__dirname, 'apps/shop')));
-    }
+// if (process.env.NODE_ENV === 'development') {
+//     if (process.env.MODE === 'no-build') {
+//         server.use(express.static(path.join(__dirname, 'apps/shop')));
+//     }
+//     else if (process.env.MODE === 'build') {
+//         server.use(express.static(path.join(__dirname, 'apps/shop')));
+//     }
    // server.use(express.static(path.join(__dirname, 'static')));
-}
+//}
 
 
 //server.use(express.static(__dirname + '/public'));
 
 server.get('/', (req, res) => {
-    if (process.env.NODE_ENV === 'developemnt') {
-        res.render('shopy', {mode: process.env.MODE});
-    }
-     else {
-        res.render('shopy');
-    }
+    // if (process.env.NODE_ENV === 'developemnt') {
+    //     res.render('index', {mode: process.env.MODE});
+    // }
+    //  else {
+        res.render('index');
+    //}
 });
 
 
