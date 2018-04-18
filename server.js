5be442adf962a1ee10e8f72cf3d8d5d1aa57f02f
walter <wjesus88@gmail.com>
2018-04-18 10:01:27 +0800

    m

--- server.js
+++ server.js
@@ -8,26 +8,26 @@ var port = process.env.PORT || 8080;
 server.set('view engine', 'ejs');
 server.set('views', 'views');
 
-// if (process.env.NODE_ENV === 'development') {
-//     if (process.env.MODE === 'no-build') {
-//         server.use(express.static(path.join(__dirname, 'apps/shop')));
-//     }
-//     else if (process.env.MODE === 'build') {
-//         server.use(express.static(path.join(__dirname, 'apps/shop/build/bundled')));
-//     }
-//     server.use(express.static(path.join(__dirname, 'static')));
-// }
+if (process.env.NODE_ENV === 'development') {
+    if (process.env.MODE === 'no-build') {
+        server.use(express.static(path.join(__dirname, 'apps/shop')));
+    }
+    else if (process.env.MODE === 'build') {
+        server.use(express.static(path.join(__dirname, 'apps/shop/build/bundled')));
+    }
+   // server.use(express.static(path.join(__dirname, 'static')));
+}
 
 
-server.use(express.static(__dirname + '/apps/shop'));
+server.use(express.static(__dirname + '/public'));
 
 server.get('/', (req, res) => {
-    // if (process.env.NODE_ENV === 'developemnt') {
-    //      res.render('shop', {mode: process.env.MODE});
-    // }
-    //  else {
-        res.render('index');
-    //}
+    if (process.env.NODE_ENV === 'developemnt') {
+         res.render('shop', {mode: process.env.MODE});
+    }
+     else {
+        res.render('shop');
+    }
 });
 
 
